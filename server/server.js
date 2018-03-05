d263ee8d3a77df25e3c473ee6484eae479d931bf
Kyle Chambers <kylejchambers@gmail.com>
2018-03-05 10:58:26 -0800

    fixed nodemon start script issue

--- server/server.js
+++ server/server.js
@@ -6,20 +6,20 @@ const database = require('../database/data.js');
 const app = express();
 const port = 3004;
 
-app.use('/listings/:id/q-and-a', express.static('../client/public'));
+app.use('/listings/:id/q-and-a', express.static(`${__dirname}/../client/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/attractions', (req, res) => {
-  database.getAll((err, data) => {
-    if (err) {
-      console.log('error from get request /attractions');
-      res.status(404).json({ message: 'No attractions' });
-    }
-    res.json(data);
-  });
-});
+// app.get('/attractions', (req, res) => {
+//   database.getAll((err, data) => {
+//     if (err) {
+//       console.log('error from get request /attractions');
+//       res.status(404).json({ message: 'No attractions' });
+//     }
+//     res.json(data);
+//   });
+// });
 
 app.get('/api/listings/:id/q-and-a', (req, res) => {
   const requestId = Number(req.params.id);
