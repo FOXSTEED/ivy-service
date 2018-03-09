a6fa8e3c3ea735bc1f8ffbc27195bf00395d82ce
Kyle Chambers <kylejchambers@gmail.com>
2018-03-08 16:33:19 -0800

    working

--- database/data.js
+++ database/data.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/27017');
-// mongoose.connect('mongodb://database/27017');
+mongoose.connect('mongodb://database/27017');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- database/seed.js
+++ database/seed.js
@@ -3,26 +3,3 @@ const database = require('./data.js');
 
 
 database.addToDb(fakeData);
-
-// function seedDb(attractions) {
-//   database.addToDb(attractions, (err, data) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     // console.log(data);
-//   });
-// }
-// seedDb(fakeData);
-
-// database.removeAll((err, data) => {
-//   if (err){
-//     console.log('error removing');
-//   }
-// });
-
-// database.getAll((err, data) => {
-//   if (err) {
-//     console.log('error from seedDB');
-//   }
-//   console.log(data);
-// });
